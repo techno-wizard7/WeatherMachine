fa76a2041431e45e541d0c2a663c9954fdc8f536
techno-wizard7 <helenamakes@proton.me>
2024-09-05 08:42:10 -0600

    Full Functionality to WeatherMachine.py and tweaked main function so it can be called by web interface

--- WeatherMachine.py
+++ WeatherMachine.py
@@ -5,45 +5,46 @@ from urllib.parse import quote_plus
 
 import requests
 
-DATE_HEADER = ["Date"]
 DATA_HEADER_BASIC = {"F": ["Date", "Weather", "Temperature(F°)", "Feels Like", "Humidity", "UV Index", "Wind Direction",
                            "Wind Speed"],
                      "C": ["Date", "Weather", "Temperature(C°)", "Feels Like", "Humidity", "UV Index", "Wind Direction",
                            "Wind Speed"]}
-DAILY_DATA_HEADER = ["Date", "Average Temperature", "Max Temperature", "Min Temperature", "UV Index"]
-HOURLY_DATA_HEADER = ["Time", "Weather", "Temperature", "Feels Like", "Temperature", "Humidity", "UV Index",
-                      "Wind Chill", "Wind Speed", "Wind Direction"]
+DAILY_DATA_HEADER = {"F": ["Date", "Average Temperature(F°)", "Max Temperature", "Min Temperature", "UV Index"],
+                     "C": ["Date", "Average Temperature(C°)", "Max Temperature", "Min Temperature", "UV Index"]} #, "", "", "", ""
+HOURLY_DATA_HEADER = {"F": ["Time", "Weather", "Temperature(F°)", "Feels Like", "Humidity", "UV Index",
+                      "Wind Chill","Wind Direction", "Wind Speed"],
+                     "C": ["Time", "Weather", "Temperature(C°)", "Feels Like", "Humidity", "UV Index",
+                      "Wind Chill", "Wind Direction", "Wind Speed"]}
 
-def getColumnWidth(data):
-    column_widths = [max(len(str(item)) for item in column) for column in zip(data)]
+def getColumnWidth(header, data):
+    column_widths = [max(len(str(item)) for item in column) for column in zip(header, data)]
     return column_widths
 
 def printBorder(column_widths):
     print("*" + "*".join("-" * (width + 2) for width in column_widths) + "*")
 
 
-def printTable(headers, data, border=2, columnWidths=0):
-    UNDERLINE = "\033[4m"
-    RESET = "\033[0m"
-    if columnWidths == 0:
-        columnWidths = [max(len(str(item)) for item in column) for column in zip(headers, *data)]
+def printTable(headers, data):
+    # UNDERLINE = "\033[4m"
+    # RESET = "\033[0m"
+    columnWidths = [max(len(str(item)) for item in column) for column in zip(headers, *data)]
     header_row = "|" + "|".join(f" {headers[i].ljust(columnWidths[i])} " for i in range(len(headers))) + "|"
     separator_row = "|" + "|".join("_" * (width + 2) for width in columnWidths) + "|"
-    if len(data) <= 1:
-        UNDERLINE, RESET = '', ''  # We don't need these if there's only one line of data
+    # if len(data) <= 1:
+    #     UNDERLINE, RESET = '', ''  # We don't need these if there's only one line of data
     data_rows = [
-        UNDERLINE + "|" + "|".join(f" {str(row[i]).ljust(columnWidths[i])} " for i in range(len(row))) + "|" + RESET
+        "|" + "|".join(f" {str(row[i]).ljust(columnWidths[i])} " for i in range(len(row))) + "|"
+        # UNDERLINE + "|" + "|".join(f" {str(row[i]).ljust(columnWidths[i])} " for i in range(len(row))) + "|" + RESET
         for row in data
     ]
     # Print the table
-    if border > 0:
-        printBorder(columnWidths)
+    printBorder(columnWidths)
     print(header_row)
     print(separator_row)
     for row in data_rows:
         print(row)
-    if border > 1:
-        printBorder(columnWidths)
+    printBorder(columnWidths)
+
 
 
 def getJsonFromUrl(url):
@@ -68,6 +69,15 @@ def roundCoordinates(input_str):
     result_str = re.sub(r"\.(?=\D|$)", "", result_str)
     return result_str
 
+def timeFormat(time):
+    mid = len(time) // 2
+    # Split the string into two halves
+    firstHalf = time[:mid]
+    secondHalf = time[mid:]
+
+    # Join the halves with a colon
+    result = firstHalf + ':' + secondHalf
+    return result
 
 def getPublicIP():
     try:
@@ -89,16 +99,14 @@ def getLocation(ip):
         return {"error": str(e)}
 
 
-# def formatDaily(weather, unit="F"):
-#     if unit == "F":
-#         speedUnits = "Miles"
-#     else:
-#         speedUnits = "Kmph"
-#
-#     data = [weather['localObsDateTime'], weather['localObsDateTime'][0], weather[f'temp{unit}'],
-#             weather[f'FeelsLike{unit}'], weather['humidity'], weather['uvindex'], weather['winddir16Point'],
-#             weather[f'windspeed{speedUnits}']]
-#     return data
+def formatDaily(weather, unit="F"):
+    if unit == "F":
+        speedUnits = "Miles"
+    else:
+        speedUnits = "Kmph"
+    data = [weather['date'], weather[f'avgtemp{unit}'],
+            weather[f'maxtemp{unit}'], weather[f'mintemp{unit}'], weather['uvIndex']]
+    return data
 
 
 def formatOneDay(weather, unit):
@@ -106,22 +114,23 @@ def formatOneDay(weather, unit):
         speedUnits = "Miles"
     else:
         speedUnits = "Kmph"
-
     data = [weather['localObsDateTime'], weather['weatherDesc'][0]['value'], weather[f'temp_{unit}'],
             weather[f'FeelsLike{unit}'], weather['humidity'], weather['uvIndex'], weather['winddir16Point'],
             weather[f'windspeed{speedUnits}']]
-    return [data]
+    return data
 
-
-def printWeather(weather, date=False, time=False, metric=False):
-    units = "F"  # °
-    if metric:
-        units = "C"
-
-    return
-
+def formatHourly(weather, unit):
+    if unit == "F":
+        speedUnits = "Miles"
+    else:
+        speedUnits = "Kmph"
+    data = [timeFormat(weather['time']), weather['weatherDesc'][0]['value'], weather[f'temp{unit}'],
+            weather[f'FeelsLike{unit}'], weather['humidity'], weather['uvIndex'], weather[f'WindChill{unit}'], weather['winddir16Point'],
+            weather[f'windspeed{speedUnits}']]
+    return data
 
 def getData(args):
+    output = []
     # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
     # fetch a weather forecast from a city
     data = getJsonFromUrl(f'https://wttr.in/{quote_plus(args.Location)}?format=j1')
@@ -134,22 +143,30 @@ def getData(args):
     location = data['nearest_area'][0]['areaName'][0]['value']
     print(f"Weather in {location}")
     if args.f:
-        size = [max(len(str(item)) for item in column) for column in zip(DATA_HEADER_BASIC[units], *data)]
+        buffer = []
         for daily in data['weather']:
-            print(daily)
-            # hourly forecasts
+            buffer.append(formatDaily(daily, units)) #.extend(["", "", "", ""])
+            printTable(DAILY_DATA_HEADER[units], buffer)
+            buffer = []
             for hourly in daily['hourly']:
-                print(f' --> {hourly!r}')
+                buffer.append(formatHourly(hourly, units))
+            printTable(HOURLY_DATA_HEADER[units], buffer)
+            buffer = []
     elif not args.f and args.H:
-        print(data['current_condition'][0])
-        # hourly forecasts
-        for hourly in data['weather']['hourly']:
-            print(f' --> {hourly!r}')
+        buffer = []
+        # buffer.append(DATA_HEADER_BASIC[units])
+        buffer.append(formatOneDay(data['current_condition'][0], units))
+        printTable(DATA_HEADER_BASIC[units], buffer)
+        buffer = []
+        for hourly in data['weather'][0]['hourly']:
+            buffer.append(formatHourly(hourly, units))
+        printTable(HOURLY_DATA_HEADER[units], buffer)
+
     else:
         data = formatOneDay(data['current_condition'][0], units)
-        size = getColumnWidth([DATA_HEADER_BASIC[units], ])
-        printTable(DATA_HEADER_BASIC[units], data, 2, size)
-    return
+        # size = getColumnWidth([DATA_HEADER_BASIC[units], ])
+        printTable(DATA_HEADER_BASIC[units], [data])
+    return output
 
 
 def getArgs(parser, input):
@@ -182,6 +199,11 @@ def getParser():
     parser.add_argument("Location", type=str, nargs='*', help="Location string (required unless -l is used)")
     return parser
 
+def run(input):
+    args = getArgs(parser, input)
+    if args.Location == "Exit":
+        raise KeyboardInterrupt
+    return getData(args)
 
 if __name__ == '__main__':
     parser = getParser()
@@ -189,11 +211,8 @@ if __name__ == '__main__':
         try:
             # Get input from the user
             input_str = input("Enter a Location and flags. Type 'Exit' to Exit")
-            # Parse the input
-            args = getArgs(parser, input_str.split())
-            if args.Location == "Exit":
-                break
-            getData(args)
+            run(input_str.split())
+
 
         except KeyboardInterrupt:
             print("\nExiting...")
